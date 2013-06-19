60ed04f420fc1b0e883e62206b2e0bb1d9d922ad
Gordon Erlebacher <gerlebacher@hpc-15-35.local>
2013-06-19 13:02:39 -0400

    Updated env file for HPC Phi node at FSU.

--- setupenv.sh
+++ setupenv.sh
@@ -22,5 +22,11 @@ elif [ $HOSTNAME = "Gordons-MacBook-Pro.local" ] ; then
 	export CL_DEVICE_TYPE="CL_DEVICE_TYPE_GPU"
 	export CLSPMVPATH=/Users/erlebach/Documents/src/spmv_mic
 	export CL_KERNELS=${CLSPMVPATH}/kernels
+elif [ $HOSTNAME = "hpc-15-35" ]  ; then
+	echo "FSU MIC/host node"
+	module load intel-oc
+	export CL_DEVICE_TYPE="CL_DEVICE_TYPE_ACCELERATOR"
+	export CLSPMVPATH=/panfs/storage.local/scs/home/gerlebacher/src/clSpMV_MIC
+	export CL_KERNELS=${CLSPMVPATH}/kernels
 fi
 
