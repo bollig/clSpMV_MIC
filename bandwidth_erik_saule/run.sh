016b2cfff74e21d93fb5a0f847f9faf1676fa367
Gordon Erlebacher <gordon.erlebach@gmail.co>
2013-07-30 14:28:00 -0500

    Makefile changed for mic.

--- bandwidth_erik_saule/run.sh
+++ bandwidth_erik_saule/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 export KMP_AFFINITY=granularity=fine,scatter
-export LD_LIBRARY_PATH=~/miclib
+#export LD_LIBRARY_PATH=~/miclib
 
 
 #for code in  memset memcpy vectsum simplesum simplesum-char memcpy memset memset-vect memset-nrngo memset-nr memset
-for code in simplesum-char-O1 simplesum-int-O1
+#for code in simplesum-char-O1 simplesum-int-O1
+for code in simplesum-int-O1
 do
-    for core in 1 2 4 8 16 24 32 40 48 56 61
+    #for core in 1 2 4 8 16 24 32 40 48 56 61
+    for core in 1 2 
     do
 	for thread in 1 2 3 4
 	do
